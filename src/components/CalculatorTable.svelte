15ae567966436657460149918df5f77b8d72505c
William Chou <williamchou@kklab.com>
2024-05-16 03:22:56 +0800

    Add CalculatorTableRow component for dynamic table rows

--- src/components/CalculatorTable.svelte
+++ src/components/CalculatorTable.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import { 手續費費率, 證券交易稅稅率 } from '../constants';
   import AppCard from './AppCard.svelte';
+  import CalculatorTableRow from './CalculatorTableRow.svelte';
 
   export let 交易類別: string;
   export let 買入價格: number;
@@ -8,31 +8,11 @@
   export let 交易股數: number;
   export let 手續費折扣: number;
   export let 最低手續費: number;
-
-  const 計算證券交易稅稅率 = (交易類別: string) => {
-    if (交易類別 === '股票當日沖銷') {
-      return 證券交易稅稅率.股票當日沖銷;
-    }
-    if (交易類別 === 'ETF') {
-      return 證券交易稅稅率.指數股票型基金;
-    }
-    return 證券交易稅稅率.股票;
-  };
-
-  $: 成本 = 買入價格 * 交易股數;
-  $: 市值 = 賣出價格 * 交易股數;
-  $: 支付總金額 = 成本 + 買入手續費;
-  $: 實收總金額 = 市值 - 賣出手續費 - 證券交易稅;
-  $: 買入手續費 = Math.max(最低手續費, 成本 * 手續費費率.證券經紀商 * 手續費折扣);
-  $: 賣出手續費 = Math.max(最低手續費, 市值 * 手續費費率.證券經紀商 * 手續費折扣);
-  $: 證券交易稅 = 市值 * 計算證券交易稅稅率(交易類別);
-  $: 損益金額 = 實收總金額 - 支付總金額;
-  $: 報酬率 = 損益金額 / 成本;
 </script>
 
 <AppCard title="試算結果">
   <div class="table-responsive">
-    <table class="table table-bordered table-striped mb-0">
+    <table class="table table-bordered table-striped text-center mb-0">
       <thead>
         <tr>
           <th>成交價格</th>
@@ -46,19 +26,9 @@
         </tr>
       </thead>
       <tbody>
-        <!-- TODO: -->
-        <!-- {#each Array.from({ length: 11 }, (_, i) => i - 5) as offset} -->
-        <tr>
-          <td>{Number(賣出價格.toFixed(1)).toLocaleString()}</td>
-          <td>{Number(支付總金額.toFixed(1)).toLocaleString()}</td>
-          <td>{Number(實收總金額.toFixed(1)).toLocaleString()}</td>
-          <td>{Number(買入手續費.toFixed(1)).toLocaleString()}</td>
-          <td>{Number(賣出手續費.toFixed(1)).toLocaleString()}</td>
-          <td>{Number(證券交易稅.toFixed(1)).toLocaleString()}</td>
-          <td>{Number(損益金額.toFixed(1)).toLocaleString()}</td>
-          <td>{Number((報酬率 * 100).toFixed(2)).toLocaleString()}%</td>
-        </tr>
-        <!-- {/each} -->
+        {#each Array.from({ length: 11 }, (_, i) => i - 5) as offset}
+          <CalculatorTableRow {交易類別} {買入價格} 賣出價格={賣出價格 + offset} {交易股數} {手續費折扣} {最低手續費} />
+        {/each}
       </tbody>
     </table>
   </div>
