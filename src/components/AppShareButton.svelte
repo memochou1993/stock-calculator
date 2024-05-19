5d4f8b10a1a26a58bbdf5a6101a38087ae9c25be
Memo Chou <memochou1993@gmail.com>
2024-05-20 01:34:03 +0800

    Add tooltip for copy button

--- src/components/AppShareButton.svelte
+++ src/components/AppShareButton.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
+  import { delay } from '$lib';
+  import type { Modal, Tooltip } from 'bootstrap';
+  import { onMount } from 'svelte';
+
   export let url: string;
 
-  const copyUrl = async () => {
-    try {
-      await navigator.clipboard.writeText(url);
-    } catch (e) {
-      //
-    }
+  let bootstrap: typeof import('bootstrap');
+
+  onMount(async () => {
+    bootstrap = await import('bootstrap');
+  });
+
+  const copy = async () => {
     if (navigator.share) {
       try {
         await navigator.share({
@@ -14,31 +19,64 @@
           text: `與你分享好用的${document.title}`,
           url,
         });
-      } catch (e) {
-        //
+      } catch (err) {
+        console.error(err);
       }
+      modal.hide();
+      return;
     }
+    try {
+      await navigator.clipboard.writeText(url);
+      showTooltip(tooltip, '複製成功！');
+    } catch (err) {
+      console.error(err);
+      showTooltip(tooltip, '複製失敗！');
+    }
+    await delay(1000);
+    modal.hide();
   };
+
+  const showTooltip = async (tooltip: Tooltip, content: string) => {
+    tooltip.setContent({ '.tooltip-inner': content });
+    tooltip.show();
+    await delay(1000);
+    tooltip.hide();
+  };
+
+  $: modal = bootstrap?.Modal.getOrCreateInstance('#modal-share') as Modal;
+  $: tooltip = bootstrap?.Tooltip.getOrCreateInstance('#tooltip-share') as Tooltip;
 </script>
 
-<button type="button" class="btn btn-variant" data-bs-toggle="modal" data-bs-target="#share-modal">
+<button type="button" class="btn btn-variant" data-bs-target="#modal-share" on:click={() => modal.show()}>
   <div class="d-flex align-items-center">
     <span class="material-symbols-outlined">share</span>
   </div>
 </button>
 
-<div class="modal fade" id="share-modal" tabindex="-1" aria-labelledby="share-modal-label" aria-hidden="true">
+<div class="modal fade" id="modal-share" tabindex="-1" aria-labelledby="modal-share-label" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content bg-body-secondary">
       <div class="modal-header">
-        <h1 class="modal-title text-variant fs-5" id="share-modal-label">分享</h1>
+        <h1 class="modal-title text-variant fs-5" id="modal-share-label">分享</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <div class="form-group mb-0">
           <input type="text" name="url" class="form-control form-control-md mb-3" bind:value={url} readonly />
           <div class="d-grid">
-            <button type="button" class="btn btn-block btn-warning" data-bs-dismiss="modal" on:click={copyUrl}>複製網址</button>
+            <button
+              class="btn btn-block btn-warning"
+              data-bs-custom-class="tooltip-variant"
+              data-bs-placement="bottom"
+              data-bs-toggle="tooltip"
+              data-bs-trigger="manual"
+              id="tooltip-share"
+              on:click={copy}
+              title="複製成功！"
+              type="button"
+            >
+              複製網址
+            </button>
           </div>
         </div>
       </div>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
 import { calculate, calculateStep, CalculatorConstant, CalculatorInput, CalculatorOutput } from './calculator';
+import { delay } from './utils';
 import { validate } from './validator';
 
-export { calculate, calculateStep, CalculatorConstant, CalculatorInput, CalculatorOutput, validate };
+export { calculate, calculateStep, CalculatorConstant, CalculatorInput, CalculatorOutput, delay, validate };
