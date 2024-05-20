2db291d1340c10b20b853fc0cf2320731dc0554a
Memo Chou <memochou1993@gmail.com>
2024-05-21 01:51:27 +0800

    Update page title and meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,28 @@
     const bootstrap = await import('bootstrap');
     document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach((el) => new bootstrap.Tooltip(el));
   });
+
+  const title = '台股交易試算器 - 投資成本、損益金額、報酬率計算工具';
+  const description = `台股交易試算器提供股票、股票當日沖銷、指數股票型基金（ETF）交易試算。藉由輸入交易類別、買入價格、賣出價格、交易股數、手續費折扣、最低手續費，可根據不同的成交價格，自動計算出股票成本、股票市值、買入手續費、賣出手續費、證券交易稅、交易成本、損益金額、報酬率。`;
+  const domain = 'https://stock.epoch.tw';
 </script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description} />
+  <meta name="keywords" content="股市, 台股, 股票, 台股交易, 股票交易, 台股試算, 股票試算, 台股計算, 股票計算, 當沖, ETF, 免費, 試算" />
+  <meta name="author" content="Memo Chou" />
+  <meta name="robots" content="index, follow" />
+  <meta name="og:title" content={title} />
+  <meta name="og:description" content={description} />
+  <meta name="og:type" content="website" />
+  <meta name="og:url" content={domain} />
+  <meta name="og:image" content="{domain}/cover.png" />
+  <meta name="twitter:title" content={title} />
+  <meta name="twitter:description" content={description} />
+  <meta name="twitter:image" content="{domain}/cover.png" />
+  <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
 
 <div class="app">
   <TheHeader />
