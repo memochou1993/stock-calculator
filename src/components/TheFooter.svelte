3a478da376976bcc28e9ba53dc2d4afdf89c1725
Memo Chou <memochou1993@gmail.com>
2024-05-22 04:41:46 +0800

    Update font links

--- src/components/TheFooter.svelte
+++ src/components/TheFooter.svelte
@@ -4,6 +4,15 @@
 </footer>
 
 <style lang="scss">
+  @font-face {
+    font-family: 'Noto Color Emoji';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url(https://fonts.gstatic.com/s/notocoloremoji/v30/Yq6P-KqIXTD0t4D9z1ESnKM3-HpFabts6diysYTngZPnMC1MfLd4gw.2.woff2) format('woff2');
+    unicode-range: U+2764;
+  }
+
   footer {
     font-family: 'Noto Color Emoji', sans-serif;
     font-size: 0.875rem;
