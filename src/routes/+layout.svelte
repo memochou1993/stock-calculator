5d32a2ea38d90b62aba9932fa65d73c898367047
Memo Chou <memochou1993@gmail.com>
2024-05-22 03:35:55 +0800

    Update meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { page } from '$app/stores';
   import { TheFooter, TheHeader } from '$components';
   import { onMount } from 'svelte';
   import { pwaInfo } from 'virtual:pwa-info';
@@ -11,7 +12,6 @@
 
   const title = '台股交易試算器 - 投資成本、損益金額、報酬率計算工具';
   const description = `台股交易試算器提供股票、股票當日沖銷、指數股票型基金（ETF）交易試算。藉由輸入交易類別、交易股數、買入價格、賣出價格、手續費折扣、最低手續費，可根據不同的成交價格，自動計算出股票成本、股票市值、買入手續費、賣出手續費、證券交易稅、交易成本、損益金額、報酬率。`;
-  const domain = 'https://stock.epoch.tw';
 
   $: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
@@ -25,11 +25,11 @@
   <meta name="og:title" content={title} />
   <meta name="og:description" content={description} />
   <meta name="og:type" content="website" />
-  <meta name="og:url" content={domain} />
-  <meta name="og:image" content="{domain}/cover.png" />
+  <meta name="og:url" content={$page.url.origin} />
+  <meta name="og:image" content="{$page.url.origin}/cover.png" />
   <meta name="twitter:title" content={title} />
   <meta name="twitter:description" content={description} />
-  <meta name="twitter:image" content="{domain}/cover.png" />
+  <meta name="twitter:image" content="{$page.url.origin}/cover.png" />
   <meta name="twitter:card" content="summary_large_image" />
   {@html webManifest}
 </svelte:head>
