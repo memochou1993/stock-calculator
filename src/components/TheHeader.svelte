7367a44cd81b1c2ba1b6459222da001cdf8c32c9
Memo Chou <memochou1993@gmail.com>
2024-05-24 03:40:47 +0800

    Rename page title

--- src/components/TheHeader.svelte
+++ src/components/TheHeader.svelte
@@ -7,14 +7,14 @@
   <div class="container-fluid">
     <div class="d-flex align-items-center">
       <img src="/favicon.png" alt="logo" class="me-2" width="24" height="24" />
-      <a class="navbar-brand text-light fs-4 py-0" href="/">台股交易試算器</a>
+      <a class="navbar-brand text-light fs-4 py-0" href="/">股票交易計算器</a>
     </div>
     <div class="d-flex">
       <button
         type="button"
         class="btn btn-dark-variant px-2"
         on:click={() => {
-          window.open('https://www.facebook.com/profile.php?id=61560002601019', '_blank', 'noopener noreferrer');
+          window.open('https://www.facebook.com/profile.php?id=61559734063696', '_blank', 'noopener noreferrer');
         }}
       >
         <AppIcon icon="flag" />
