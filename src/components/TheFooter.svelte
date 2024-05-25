eeb631bfd380ad9caa77d396eb4e6ce455d77af5
Memo Chou <memochou1993@gmail.com>
2024-05-25 22:28:26 +0800

    Update footer

--- src/components/TheFooter.svelte
+++ src/components/TheFooter.svelte
@@ -2,18 +2,21 @@
   import { GTM } from '$lib';
 </script>
 
-<footer class="text-center py-5">
-  Built with ❤️ by
-  <a
-    href="https://profile.epoch.tw/"
-    on:click={() => {
-      GTM.pushEvent('visit_author_profile');
-    }}
-    rel="noopener noreferrer"
-    target="_blank"
-  >
-    Memo Chou
-  </a>
+<footer class="text-center my-5 py-3">
+  <div>
+    &copy; {new Date().getFullYear()}
+    <a
+      class="text-body text-decoration-none"
+      href="https://profile.epoch.tw/"
+      on:click={() => {
+        GTM.pushEvent('visit_author_profile');
+      }}
+      rel="noopener noreferrer"
+      target="_blank"
+    >
+      Memo Chou
+    </a>
+  </div>
 </footer>
 
 <style lang="scss">
