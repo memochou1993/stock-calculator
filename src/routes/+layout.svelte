08c6e673a53a2430daa0f8fe6d2868e0c3dab06d
Memo Chou <memochou1993@gmail.com>
2024-05-26 17:14:13 +0800

    Add url env

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  import { page } from '$app/stores';
   import { TheFooter, TheHeader } from '$components';
   import { onMount } from 'svelte';
   import './layout.scss';
   // @ts-expect-error
   import { pwaInfo } from 'virtual:pwa-info';
 
+  const url = import.meta.env.VITE_APP_URL;
   const title = '股票交易計算器｜手續費、損益金額、報酬率計算工具';
   const description = `股票交易計算器，提供股票、股票當日沖銷、指數股票型基金（ETF）交易試算。輸入交易類別、交易股數、買入價格、賣出價格、手續費折扣、最低手續費，可根據不同的成交價格，自動計算出股票成本、股票市值、買入手續費、賣出手續費、證券交易稅、交易成本、損益金額、報酬率。`;
 
@@ -34,11 +34,11 @@
   <meta name="og:title" content={title} />
   <meta name="og:description" content={description} />
   <meta name="og:type" content="website" />
-  <meta name="og:url" content={$page.url.origin} />
-  <meta name="og:image" content="{$page.url.origin}/cover.png" />
+  <meta name="og:url" content={url} />
+  <meta name="og:image" content="{url}/cover.png" />
   <meta name="twitter:title" content={title} />
   <meta name="twitter:description" content={description} />
-  <meta name="twitter:image" content="{$page.url.origin}/cover.png" />
+  <meta name="twitter:image" content="{url}/cover.png" />
   <meta name="twitter:card" content="summary_large_image" />
   {@html webManifest}
 </svelte:head>
